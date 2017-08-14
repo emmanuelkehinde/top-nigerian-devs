8e349e179ba868eafb79b575da885a517a4f7e99
Kehinde Emmanuel <emmakoko1996@gmail.com>
2017-08-14 22:28:40 +0100

    Add Router Link to home menu

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '8080';
+const port = process.env.PORT || '3000';
 app.set('port', port);
 
 /**
